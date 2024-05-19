95a9c5059f35af7569052f50a3aa3f23fa353dcf
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2024-05-19 21:50:59 +0200

    Adicao das cores do app

---
+++ lib/data/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//Cores
+const bgCollor = Color(0xfff1F212C);
+const whiteColor = Color(0xffFFFFFF);
+const sliderColor = Color(0xff7E70FF);
+const buttonColor = Color(0xff60E950);
+const bgDarkColor = Color(0xff070B11);
+
